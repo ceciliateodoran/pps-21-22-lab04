9a258c077526c8c8cc6701888a76d8f5a6973fe6
CeciliaTeodorani <cecilia.teodorani@studio.unibo.it>
2022-03-24 11:55:35 +0100

    a05b logics implementation - es3

--- src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
@@ -1,9 +1,23 @@
 package u04lab.polyglot.a05b
 
+import u04lab.polyglot.Pair
+import scala.util.*
+
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
 
-  override def isOver: Boolean = false
+  private var tickCount: Int = 0
+  private val random = Random()
+  private val initial = Pair[Int, Int](random.nextInt(size - 2) + 1, random.nextInt(size - 2) + 1)
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def tick(): Unit = tickCount = tickCount + 1
+
+  override def isOver: Boolean =
+    initial.getY - tickCount < 0 || initial.getY + tickCount >= size ||
+      initial.getX - tickCount < 0 || initial.getX + tickCount >= size
+
+  override def hasElement(x: Int, y: Int): Boolean =
+    (x == initial.getX && Math.abs(y - initial.getY) <= tickCount) ||
+    (y == initial.getY && Math.abs(x - initial.getX) <= tickCount) ||
+    (x - y == initial.getX - initial.getY && Math.abs(x - initial.getX) <= tickCount) ||
+    (x + y == initial.getX + initial.getY && Math.abs(x - initial.getX) <= tickCount)
